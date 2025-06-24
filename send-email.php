254d36cb898b248765edcad9d28ed53542280a2f
Gybrand <fikliag10@gmail.com>
2025-06-24 07:45:14 +0700

    new

--- send-email.php
+++ send-email.php
@@ -16,7 +16,7 @@ function kirimEmailDeadlineHariIni($dataTugas) {
 
         // Ganti dengan email dan App Password kamu
         $mail->Username   = 'etemporary888@gmail.com'; 
-        $mail->Password   = 'nyjbaevkepqgwoor'; // App Password
+        $mail->Password   = '1ddad12123123123'; // App Password
 
         $mail->SMTPSecure = 'tls';
         $mail->Port       = 587;
